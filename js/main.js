55d65bf72f71c40b4a3ed7a84d9692ceecc0563f
Maitland Corker <m.corker@LIVE.CO.UK>
2020-11-09 09:37:25 +0000

    colour

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
     //Start of the code
     console.info("Hello Console");
     console.dir(document);
-    document.getElementById("myHeading").innerHTML = "Maitland's Website"
+    document.getElementById("myHeading").innerHTML = "Maitland's Website";
+    document.querySelector("nav li").setAttribute("class", "currentPage");
     // The end of it
 })();
 

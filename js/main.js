d5b64b4c3ed47d019038c966856de36c56f162ae
Maitland Corker <m.corker@LIVE.CO.UK>
2020-11-09 09:41:02 +0000

    google

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
     console.dir(document);
     document.getElementById("myHeading").innerHTML = "Maitland's Website";
     document.querySelector("nav li").setAttribute("class", "currentPage");
+    document.querySelector("nav ul li a").setAttribute('href','http://www.google.co.uk');
     // The end of it
 })();
 

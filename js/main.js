3736bb16f7e3394d3322e169a4440b686febca8a
Maitland Corker <m.corker@LIVE.CO.UK>
2020-11-09 09:34:03 +0000

    iife

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-console.info("Hello Console");
-console.dir(document);
-document.getElementById("myHeading").innerHTML = "Maitland's Website"
+//IIFE
+(function(){
+    //Start of the code
+    console.info("Hello Console");
+    console.dir(document);
+    document.getElementById("myHeading").innerHTML = "Maitland's Website"
+    // The end of it
+})();
+
